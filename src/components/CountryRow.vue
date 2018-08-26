c9c1cbdbc301637b1177481d0e8d951f6ec68e43
acrossoffwest <acrossoffwest@acrossoffwest-debian.aow.space>
2018-08-27 01:02:01 +0200

    (fix) Small fixes

---
+++ src/components/CountryRow.vue
@@ -0,0 +1,48 @@
+<template>
+    <tr>
+        <td>{{ country }}</td>
+        <day-buttons
+                v-for="dayIndex in countDays"
+                :key="dayIndex"
+                :day-data="getDayData(dayIndex)"
+        />
+    </tr>
+</template>
+
+<script>
+    import DayButtons from './DayButtons.vue'
+
+    export default {
+        name: 'CountryRow',
+        components: {
+          DayButtons
+        },
+        props: {
+            current: {
+                required: true,
+                type: Object
+            },
+            country: {
+                required: true,
+                type: String
+            },
+            countryData: {
+                required: true,
+                type: Object
+            }
+        },
+        computed: {
+          countDays() {
+            return this.getCurrentDate().daysInMonth()
+          }
+        },
+        methods: {
+          getDayData(dayIndex = 1) {
+            return this.countryData[this.getCurrentDate(dayIndex).format('YYYY-MM-DD')]
+          },
+          getCurrentDate(dayIndex = 1) {
+            return this.$moment(`${this.current.year}-${this.current.month}-${dayIndex}`, 'YYYY-MM-DD')
+          }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import moment from 'moment'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$moment = moment
 
 new Vue({
   render: h => h(App)
