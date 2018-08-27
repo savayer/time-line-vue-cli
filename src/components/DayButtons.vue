d89d885398c6a13be49d6017512e120dec0c09af
savayer <sava0094@gmail.com>
2018-08-27 10:57:57 +0700

    added styles for define free day

--- src/components/DayButtons.vue
+++ src/components/DayButtons.vue
@@ -1,8 +1,8 @@
 <template>
     <td>
-        <button class="btn btn-info mybtn morning" :title="getTitle(dayData.morning)"></button>
-        <button class="btn btn-primary mybtn day" :title="getTitle(dayData.day)"></button>
-        <button class="btn btn-secondary mybtn evening" :title="getTitle(dayData.evening)"></button>
+        <button class="btn btn-info mybtn morning" :title="getTitle(dayData.morning)" :data-busy="isTitle(dayData.morning)"></button>
+        <button class="btn btn-primary mybtn day" :title="getTitle(dayData.day)" :data-busy="isTitle(dayData.morning)"></button>
+        <button class="btn btn-secondary mybtn evening" :title="getTitle(dayData.evening)" :data-busy="isTitle(dayData.morning)"></button>
     </td>
 </template>
 
@@ -24,7 +24,11 @@
         methods: {
           getTitle(event = {}) {
             return event.title
+          },
+          isTitle(event = {}) {
+            return event.title === undefined ? 0 : 1;
           }
+
         }
     }
 </script>
